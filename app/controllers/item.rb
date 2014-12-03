97c5d9cc74820101107a8c72aae24989586e929d
Beatrix "Betsy" Carroll <beatrixcarroll@gmail.com>
2014-12-03 15:00:49 -0500

    add new item is working

--- app/controllers/item.rb
+++ app/controllers/item.rb
@@ -3,6 +3,31 @@ get '/item/all' do
   erb :'/item/all'
 end
 
+#upload a new item
+get '/item/new' do
+  if current_user
+    erb :'item/new_form'
+  else
+    redirect("/login")
+  end
+end
+
+post '/item' do
+  p "**********"
+  p params[:item]
+  item = Item.new(params[:item])
+  item.user_id = current_user.id
+  tag = Tag.new(params[:tag])
+  item.tags << tag
+
+  if item.save
+    redirect("/item/#{item.id}")
+  else
+    session[:error] = item.errors.messages
+    redirect("/item/new")
+  end
+end
+
 get '/item/:id' do
   if session[:user_id]
     @item = Item.find(params[:id])
@@ -10,26 +35,5 @@ get '/item/:id' do
     erb :'/item/show'
   else
     redirect('/user/login')
-  end
-end
-
-#upload a new item
-get '/item/new' do
-  if current_user
-    erb :new_form
-  else
-    redirect("/login")
-  end
-end
-
-post '/item' do
-  item = Item.new(params[:entry])
-  tag = Tag.new(params[:tag])
-  item.tags << tag
-  if entry.save
-    redirect("/item/#{item.id}")
-  else
-    session[:error] = item.errors.messages
-    redirect("/item/new")
   end
 end
--- db/migrate/20141202194003_create_item_tag_relationship.rb
+++
@@ -1,8 +0,0 @@
-class CreateItemTagRelationship < ActiveRecord::Migration
-  def change
-    create_table :item_tag_relationship do |t|
-      t.references :tag
-      t.references :item
-    end
-  end
-end
